4348f0e345fb94cd099bdaa1263a35077bc04beb
yozz <yozz.online@gmail.com>
2015-09-07 09:59:28 -0400

    Flush permalinks on plugin activation

--- startup-cpt-testimonials.php
+++ startup-cpt-testimonials.php
@@ -73,6 +73,14 @@ function startup_reloaded_testimonials() {
 
 add_action( 'init', 'startup_reloaded_testimonials', 0 );
 
+//Flusher les permalink à l'activation du plgin pour qu'ils fonctionnent sans mise à jour manuelle
+function startup_reloaded_testimonials_rewrite_flush() {
+    startup_reloaded_testimonials();
+    flush_rewrite_rules();
+}
+
+register_activation_hook( __FILE__, 'startup_reloaded_testimonials_rewrite_flush' );
+
 // Capabilities
 function startup_reloaded_testimonials_caps() {	
 	$role_admin = get_role( 'administrator' );
