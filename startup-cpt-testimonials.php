60869ab427996da13665c9a9f666c7d1426b2da3
yozz <yozz.online@gmail.com>
2015-11-18 14:50:20 -0500

    Disabled Github Updater

--- startup-cpt-testimonials.php
+++ startup-cpt-testimonials.php
@@ -29,7 +29,7 @@ function startup_reloaded_testimonials_updater() {
 	}
 }
 
-add_action( 'init', 'startup_reloaded_testimonials_updater' );
+//add_action( 'init', 'startup_reloaded_testimonials_updater' );
 
 //CPT
 function startup_reloaded_testimonials() {
