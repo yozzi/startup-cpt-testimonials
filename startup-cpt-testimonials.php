8d0da1721137e68739d6cae435c736d4171b909f
yozz <yozz.online@gmail.com>
2015-11-24 09:31:08 -0500

    Security

--- startup-cpt-testimonials.php
+++ startup-cpt-testimonials.php
@@ -7,6 +7,11 @@ Version: 1.0.0
 Text Domain: startup-cpt-testimonials
 */
 
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
 //GitHub Plugin Updater
 function startup_reloaded_testimonials_updater() {
 	include_once 'lib/updater.php';
