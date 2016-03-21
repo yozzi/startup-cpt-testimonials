fabeeef3d426e46e79c469e30627bcbf3c16f093
yozz <yozz.online@gmail.com>
2016-03-21 09:28:19 -0400

    Minor

--- startup-cpt-testimonials.php
+++ startup-cpt-testimonials.php
@@ -129,7 +129,7 @@ add_shortcode( 'testimonials', 'startup_cpt_testimonials_shortcode' );
 
 // Shortcode UI
 /**
- * Detecion de Shortcake. Identique dans tous les plugins.
+ * Detection de Shortcake. Identique dans tous les plugins.
  */
 if ( !function_exists( 'shortcode_ui_detection' ) ) {
     function shortcode_ui_detection() {
